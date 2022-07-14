994fe08574f6b8163eaec6a60fe4776fcfe577e1
vijayvky <prabha.vky@gmail.com>
2022-07-14 19:19:06 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,63 +1,63 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AddTweetComponent } from './add-tweet/add-tweet.component';
-import { AllUsersComponent } from './all-users/all-users.component';
-import { AuthGuardGuard } from './auth-guard.guard';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { LoginComponent } from './login/login.component';
-import { MyTweetsComponent } from './my-tweets/my-tweets.component';
-import { RegisterComponent } from './register/register.component';
-import { TweetsComponent } from './tweets/tweets.component';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UserSearchComponent } from './user-search/user-search.component';
-import { UserTweetComponent } from './user-tweet/user-tweet.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AddTweetComponent } from "./add-tweet/add-tweet.component";
+import { AllUsersComponent } from "./all-users/all-users.component";
+import { AuthGuardGuard } from "./auth-guard.guard";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { LoginComponent } from "./login/login.component";
+import { MyTweetsComponent } from "./my-tweets/my-tweets.component";
+import { RegisterComponent } from "./register/register.component";
+import { TweetsComponent } from "./tweets/tweets.component";
+import { UserDashboardComponent } from "./user-dashboard/user-dashboard.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { UserSearchComponent } from "./user-search/user-search.component";
+import { UserTweetComponent } from "./user-tweet/user-tweet.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
   {
-    path: 'home',
+    path: "home",
     component: UserDashboardComponent,
     canActivate: [AuthGuardGuard],
   },
   {
-    path: 'all-tweets',
+    path: "all-tweets",
     component: TweetsComponent,
     canActivate: [AuthGuardGuard],
   },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: "forgot-password", component: ForgotPasswordComponent },
   {
-    path: 'my-tweets',
+    path: "my-tweets",
     component: MyTweetsComponent,
     canActivate: [AuthGuardGuard],
   },
   {
-    path: 'all-users',
+    path: "all-users",
     component: AllUsersComponent,
     canActivate: [AuthGuardGuard],
   },
-  { path: 'search/:userName', component: UserSearchComponent },
+  { path: "search/:userName", component: UserSearchComponent },
   {
-    path: 'add-tweet',
+    path: "add-tweet",
     component: AddTweetComponent,
     canActivate: [AuthGuardGuard],
   },
   {
-    path: 'tweet/:tweetId',
+    path: "tweet/:tweetId",
     component: UserTweetComponent,
     canActivate: [AuthGuardGuard],
   },
   {
-    path: 'profile',
+    path: "profile",
     component: UserProfileComponent,
     canActivate: [AuthGuardGuard],
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
